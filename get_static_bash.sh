f4809e81995fd682fba02f397c01a46314729431
Björn Grüning <bjoern@gruenings.eu>
2018-02-03 15:49:49 +0100

    Revert "Update base Biocontainers image"

---
+++ get_static_bash.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+mkdir -p ./bin
+docker run -v `pwd`/bin:/store ubuntu:12.04 /bin/bash -c "apt-get -y update && apt-get -y install bash-static && cp /bin/bash-static /store/"
+mv ./bin/bash-static ./bin/bash
